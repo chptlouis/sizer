7a69a34862927384d821a21525fec12e34c4221f
chptlouis <louis.chopinet@epitech.eu>
2023-10-02 22:14:51 +0200

    ~ commented/uncommented good code to use local database

--- src/App.js
+++ src/App.js
@@ -4,34 +4,34 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [pairs, setPairs] = useState([]);
+  // const [pairs, setPairs] = useState([]);
 
-  useEffect(() => {
-    const fetchPairs = async () => {
-      const res = await axios.get('http://localhost:3000/pairs');
-      setPairs(res.data);
-    }
-    fetchPairs();
-  }, []);
+  // useEffect(() => {
+  //   const fetchPairs = async () => {
+  //     const res = await axios.get('http://localhost:3000/pairs');
+  //     setPairs(res.data);
+  //   }
+  //   fetchPairs();
+  // }, []);
 
-  // const pairs = [
-  //     ['XAUUSD', 100],
-  //     ['XAGUSD', 5000],
-  //     ['EURUSD', 100000],
-  //     ['EURAUD', 100000],
-  //     ['EURCAD', 100000],
-  //     ['EURCHF', 100000],
-  //     ['GBPUSD', 100000],
-  //     ['EURGBP', 100000],
-  //     ['EURJPY', 100000],
-  //     ['USDJPY', 100000],
-  //     ['AUDUSD', 100000],
-  //     ['NZDUSD', 100000],
-  //     ['NDQ100', 10],
-  //     ['SPX500', 10],
-  //     ['GER30', 10],
-  //     ['AUDCAD', 100000],
-  // ];
+  const pairs = [
+      ['XAUUSD', 100],
+      ['XAGUSD', 5000],
+      ['EURUSD', 100000],
+      ['EURAUD', 100000],
+      ['EURCAD', 100000],
+      ['EURCHF', 100000],
+      ['GBPUSD', 100000],
+      ['EURGBP', 100000],
+      ['EURJPY', 100000],
+      ['USDJPY', 100000],
+      ['AUDUSD', 100000],
+      ['NZDUSD', 100000],
+      ['NDQ100', 10],
+      ['SPX500', 10],
+      ['GER30', 10],
+      ['AUDCAD', 100000],
+  ];
 
   return (
     <div>
